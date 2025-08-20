c0eae4db8f138e8baecc5012e4cacc24a68dd231
tujandred <1098512+tujandred@users.noreply.github.com>
2025-08-20 12:19:46 +0200

    modificaciones iniciales

--- src/main/java/io/github/tujandred/DemoDTO2/TestDTO.java
+++ src/main/java/io/github/tujandred/DemoDTO2/TestDTO.java
@@ -1,4 +1,14 @@
 package io.github.tujandred.DemoDTO2;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class TestDTO {
+    private Long id = 0L;
+    private String nombre = "Nombre de prueba";
+    private String descripcion = "Descripcion de prueba";
 }
